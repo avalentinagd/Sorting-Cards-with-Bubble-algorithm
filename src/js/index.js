daa51bc989710f6846a377ea444158eea99b934e
avalentinagd <avalentinagd@gmail.com>
2020-07-27 14:04:43 +0000

    Random Card Dealer

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,21 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let middle = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let rmiddle = Math.floor(Math.random() * middle.length);
+  let x = document.querySelector("#middle");
+  x.innerHTML = middle[rmiddle];
+
+  let simbol = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+  let rsimbol = Math.floor(Math.random() * simbol.length);
+  let y = document.querySelector("#up");
+  y.innerHTML = simbol[rsimbol];
+
+  let z = document.querySelector("#down");
+  z.innerHTML = simbol[rsimbol];
+
+  if (simbol[rsimbol] == "&hearts;" || simbol[rsimbol] == "&diams;") {
+    y.style.color = "red";
+    z.style.color = "red";
+  }
 };
